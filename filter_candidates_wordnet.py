7a77bde0e383f5239891b393fefbcbdc6573fa79
mschild <matthiass2006@web.de>
2018-10-29 14:00:50 +0100

    added wornet candidate filter

---
+++ filter_candidates_wordnet.py
@@ -0,0 +1,36 @@
+from nltk.corpus import wordnet as wn
+import queue
+
+
+
+def filter_candidates_wordnet(comparison_object, candidates):
+    hypernyms_comparison_object = get_all_hypernyms(comparison_object)
+    print(hypernyms_comparison_object)
+    filtered_candicates = []
+    for candidate in candidates:
+        print('---', candidate, '---')
+
+        candidate_hypernyms = get_all_hypernyms(candidate[0])
+        print(candidate_hypernyms)
+        if not set(hypernyms_comparison_object).isdisjoint(candidate_hypernyms):
+            filtered_candicates.append(candidate)
+
+    return filtered_candicates
+
+
+
+def get_all_hypernyms(comparison_object):
+    q = queue.Queue()
+
+    for ss in wn.synsets(comparison_object):
+        for hypernym in ss.hypernyms():
+            q.put(hypernym)
+
+    hypernyms = []
+    while not q.empty():
+        hypernym = q.get()
+        hypernyms.append(hypernym)
+        for hypernym in hypernym.hypernyms():
+            q.put(hypernym)
+
+    return hypernyms
