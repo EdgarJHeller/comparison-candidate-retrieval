448c63e6d06828c172e28171dea8c9c2435bf5f4
mschild <matthiass2006@web.de>
2018-10-29 12:14:34 +0100

    changed extraction of candidates to noun phrase based

--- extract_candidates.py
+++ extract_candidates.py
@@ -1,22 +1,23 @@
 import nltk
 import re
 import operator
+from textblob import TextBlob
 
 
 def extract_candidates(comparison_object, sentences):
     unique_candidates = {}
     print(len(sentences))
     for sentence in sentences:
-        taglist = tag_sentence(sentence)
-        for tag in taglist:
-            if tag[1].startswith('NN'):
-                candidate = tag[0].lower()
-                if candidate not in [comparison_object, 'vs', 'vs.'] and is_candidate(candidate, comparison_object, sentence):
 
-                    if candidate in unique_candidates:
-                        unique_candidates[candidate] += 1
-                    else:
-                        unique_candidates[candidate] = 1
+        blob = TextBlob(sentence)
+
+        for candidate in blob.noun_phrases:
+            if candidate not in [comparison_object, 'vs', 'vs.'] and is_candidate(candidate, comparison_object, sentence):
+
+                if candidate in unique_candidates:
+                    unique_candidates[candidate] += 1
+                else:
+                    unique_candidates[candidate] = 1
 
     unique_candidates = sorted(unique_candidates.items(), key=operator.itemgetter(1), reverse=True)
     return unique_candidates
@@ -25,10 +26,12 @@ def extract_candidates(comparison_object, sentences):
 def is_candidate(candidate, comparison_object, sentence):
     vs = ' (vs|vs.) '
     candidate = re.escape(candidate)
-    pattern = '(' + candidate + vs + comparison_object + '|' + comparison_object + vs + candidate + ')'
+    pattern = '(' + candidate + vs + comparison_object + \
+        '|' + comparison_object + vs + candidate + ')'
     if re.match(pattern, sentence, re.IGNORECASE) is not None:
         # print(sentence)
         return True
+
 
 def tag_sentence(sentence):
     '''
