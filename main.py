1d921ea5e8db7e26220a5393f48ca0bfc0b108e3
mschild <matthiass2006@web.de>
2018-11-01 16:44:40 +0100

    refactoring; adding DT filter

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ import query_sentences
 import extract_candidates
 import filter_candidates_wordnet
 import filter_candidates_classifier
+import filter_candidates_DT
 
 
 if __name__ == "__main__":
 
-    comparison_object = 'python'
+    comparison_object = 'Python'
     sentences = query_sentences.retrieve_sentences(comparison_object)
     candicates = extract_candidates.extract_candidates(
         comparison_object, sentences)
     print('Candidates: ')
-    print(candicates)
-    # filtered_candidates = filter_candidates_wordnet.filter_candidates_wordnet(comparison_object, candicates)
-    filtered_candidates = filter_candidates_classifier.filter(
-        comparison_object, candicates)
+    print([c[0] for c in candicates])
+    # filtered_candidates = filter_candidates_wordnet.filter(comparison_object, candicates)
+    # filtered_candidates = filter_candidates_classifier.filter(comparison_object, candicates)
+    filtered_candidates = filter_candidates_DT.filter(comparison_object, candicates)
 
-    print(filtered_candidates)
+    # print(filtered_candidates)
     print('-----------------')
-    result = [candidate[0][0] for candidate in filtered_candidates]
-    print(result)
+    # result = [candidate[0][0] for candidate in filtered_candidates]
+    # print(result)
     # print(filtered_candidates)
 
