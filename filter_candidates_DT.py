cf7894a5281431697499a203748144372a881285
mschild <matthiass2006@web.de>
2018-11-01 19:29:07 +0100

    added filtering based on DT

--- filter_candidates_DT.py
+++ filter_candidates_DT.py
@@ -1,36 +1,31 @@
-from nltk.corpus import wordnet as wn
-import queue
+from elasticsearch import Elasticsearch
+import re
 
+ES_HOST = {"host" : "localhost", "port" : 9200}
+INDEX_NAME = 'dt-index'
 
+es = Elasticsearch(hosts = [ES_HOST], timeout=300)
 
 def filter(comparison_object, candidates):
-    hypernyms_comparison_object = get_all_hypernyms(comparison_object)
-    print(hypernyms_comparison_object)
+    similarities_comparison_object = get_all_similarities(comparison_object)
     filtered_candidates = []
     for candidate in candidates:
         print('---', candidate, '---')
 
-        candidate_hypernyms = get_all_hypernyms(candidate[0])
-        print(candidate_hypernyms)
-        if not set(hypernyms_comparison_object).isdisjoint(candidate_hypernyms):
-            filtered_candidates.append(candidate)
+        if any(candidate[0] == s or candidate[0] == re.sub('[^a-zA-Z0-9 ]', '', s)  for s in similarities_comparison_object):
+            filtered_candidates.append(candidate[0])    
+        
+        # print([s for s in similarities_comparison_object if candidate[0] == s])
+
 
     return filtered_candidates
 
 
 
-def get_all_hypernyms(comparison_object):
-    q = queue.Queue()
+def get_all_similarities(comparison_object):
+    print(comparison_object)
+    res = es.search(index = INDEX_NAME, size=10000, body={"query": {"match": {"first": comparison_object}}})
 
-    for ss in wn.synsets(comparison_object):
-        for hypernym in ss.hypernyms():
-            q.put(hypernym)
+    # similar_words = [re.sub('[^a-zA-Z0-9 ]', '', hit['_source']['second']) for hit in res['hits']['hits']]
 
-    hypernyms = []
-    while not q.empty():
-        hypernym = q.get()
-        hypernyms.append(hypernym)
-        for hypernym in hypernym.hypernyms():
-            q.put(hypernym)
-
-    return hypernyms
+    return [hit['_source']['second'] for hit in res['hits']['hits']]
