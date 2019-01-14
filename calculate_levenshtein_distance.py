6be30b48c92345a2863ef10eb25364ca5fa467df
edgarjheller <hauke.heller@gmail.com>
2019-01-14 14:55:23 +0100

    details are written to evaluation csv

--- calculate_levenshtein_distance.py
+++ calculate_levenshtein_distance.py
@@ -6,7 +6,6 @@ def get_distance_matrix(str_list1, str_list2):
     """ Construct a levenshtein distance matrix for a list of strings"""
     dist_matrix = np.zeros(shape=(len(str_list1), len(str_list2)))
     t0 = time()
-    print("Starting to build distance matrix. This will iterate from 0 till ", len(str_list1)) 
     for i in range(0, len(str_list1)):
         for j in range(0, len(str_list2)):
                 dist_matrix[i][j] = distance(str_list1[i], str_list2[j])
@@ -15,12 +14,3 @@ def get_distance_matrix(str_list1, str_list2):
     print("took", (t1-t0), "seconds")
     # sum of each rows minimum of dist_matrix
     return(dist_matrix)
-    
-
-#str_list = ["analyze", "analyse", "analysis", "analyst", "analyze"]
-
-#ggl_list = ['ios','windows','apple emojis','ios marktanteil','ios security','ios emojis','chrome os','iphone meme','apple meme','apple']
-#ccr_list = ['apple', 'piccolo', 'windows', 'blackberry', 'player', 'chrome', 'mac', 'bb', 'amazon', 'limo']
-
-#get_distance_matrix(ggl_list, ccr_list)
-
--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import csv
 import sys
 import calculate_levenshtein_distance
 import query_ggl_suggestions
+import numpy as np
 
 
 CANDIDATES_CSV_PATH = './results/candidates.csv'
@@ -23,67 +24,60 @@ def get_percent_contained(comparison_object, selected_candidates):
 
 
 if __name__ == "__main__":
-    # write headlines to evaluation csv
-    with open(EVALUATION_CSV_PATH, 'a', newline='', encoding="UTF-8") as f:
-        writer = csv.writer(f)
-        writer.writerows([['whithout', 'wordnet', 'classifier', 'DT']])
-    
     # write headlines to candidates csv
     with open(CANDIDATES_CSV_PATH, 'a', newline='', encoding="UTF-8") as f:
         writer = csv.writer(f)
         writer.writerows([['Comparison Object:', 'Candidates:']])
 
+    # write headlines to evaluation csv
+    with open(EVALUATION_CSV_PATH, 'a', newline='', encoding="UTF-8") as f:
+        writer = csv.writer(f)
+        writer.writerows([['ccr vs google', 'Comparison Object:', 'Levenshtein Distance Minima', 'LDM Sum', 'Matching Suggestions Sum']])
+
     # the comparison object is now set as third parameter when main is called
     # eg: python main.py reader reader python -> comparison object is "python"
-    comparison_object = sys.argv[3]
+    #comparison_object = sys.argv[3]
     
-    # sentences is a list with sentenses that contain the comparison_object AND vs
-    sentences = query_sentences.retrieve_sentences(comparison_object)
-    candidates = extract_candidates.extract_candidates(comparison_object, sentences)
+    for comparison_object in object_lists.objects:
 
-    wordnet_filtered_candidates = filter_candidates_wordnet.filter(comparison_object, candidates)
-    
-    while len(wordnet_filtered_candidates) < 10:
-        wordnet_filtered_candidates.append('')
+        # sentences is a list with sentenses that contain the comparison_object AND vs
+        sentences = query_sentences.retrieve_sentences(comparison_object)
+        candidates = extract_candidates.extract_candidates(comparison_object, sentences)
 
-    print('---------', comparison_object ,'---------')
-    print(wordnet_filtered_candidates)
-    
-    # the suggestions as a list of lists
-    ggl_suggestions = query_ggl_suggestions.get_suggestions(comparison_object + ' vs')
-    # the suggestions of a list of strings
-    ggl_suggestions = [item[0] for item in ggl_suggestions]
-    # each suggestions stripped of the comparison object and the 'vs'
-    ggl_suggestions = [s[len(comparison_object + ' vs '): ] for s in ggl_suggestions]
-    print(ggl_suggestions)
+        wordnet_filtered_candidates = filter_candidates_wordnet.filter(comparison_object, candidates)
+        
+        while len(wordnet_filtered_candidates) < 10:
+            wordnet_filtered_candidates.append('')
 
-    dist_matrix = calculate_levenshtein_distance.get_distance_matrix(ggl_suggestions, wordnet_filtered_candidates)
-    levenshtein_distance_minima = dist_matrix.min(1)
-    print(levenshtein_distance_minima)
-    print(levenshtein_distance_minima.sum())
-    
+        print('---------', comparison_object ,'---------')
+        print(wordnet_filtered_candidates)
+        
+        # the suggestions as a list of lists
+        ggl_suggestions = query_ggl_suggestions.get_suggestions(comparison_object + ' vs')
+        # the suggestions of a list of strings
+        ggl_suggestions = [item[0] for item in ggl_suggestions]
+        # each suggestions stripped of the comparison object and the 'vs'
+        ggl_suggestions = [s[len(comparison_object + ' vs '): ] for s in ggl_suggestions]
+        print(ggl_suggestions)
+
+        # levenshtein distance matrix (LDM) between google suggestions and ccr suggestions from commoncrawl
+        dist_matrix = calculate_levenshtein_distance.get_distance_matrix(ggl_suggestions, wordnet_filtered_candidates)
+        # minima of each column of LDM
+        levenshtein_distance_minima = dist_matrix.min(0)
+        print(levenshtein_distance_minima)
+        # sum of LCM minima
+        lcm_sum = np.sum(levenshtein_distance_minima)
+        
+        matching_suggestions_sum = str(10 - np.count_nonzero(levenshtein_distance_minima))
+        print('matches: '+ matching_suggestions_sum)
 
 
-
-    with open(CANDIDATES_CSV_PATH, 'a', newline='', encoding="UTF-8") as f:
-        writer = csv.writer(f)
-        if not wordnet_filtered_candidates:
-            writer.writerows([[comparison_object]])
-        else:
+        with open(CANDIDATES_CSV_PATH, 'a', newline='', encoding="UTF-8") as f:
+            writer = csv.writer(f)
             writer.writerow(['ccr'] + [comparison_object] + wordnet_filtered_candidates)
             writer.writerow(['ggl'] + [comparison_object] + ggl_suggestions)
 
 
-'''
-        results = [[get_percent_contained(
-                        comparison_object, first_ten_candidates),
-                    get_percent_contained(
-                        comparison_object, wordnet_filtered_candidates),
-                    get_percent_contained(
-                        comparison_object, classifier_filtered_candidates),
-                    get_percent_contained(comparison_object, DT_filtered_candidates)]]
-
         with open(EVALUATION_CSV_PATH, 'a', newline='', encoding="UTF-8") as f:
             writer = csv.writer(f)
-            writer.writerows(results)
-'''
+            writer.writerow(['ccr vs google'] + [comparison_object] + [levenshtein_distance_minima] + [lcm_sum] + [matching_suggestions_sum])
